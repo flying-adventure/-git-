666a5c5f76380a0706a44a82ac31e0d18aa11f21
Southernyj <143931628+Southernyj@users.noreply.github.com>
2023-12-12 22:38:02 +0900

    Update opencv_facecrop.py

--- cv_env/opencv_facecrop.py
+++ cv_env/opencv_facecrop.py
@@ -3,7 +3,7 @@ import cv2
 face_cascade = cv2.CascadeClassifier('../haarcascades/haarcascade_frontalface_default.xml')
 eye_casecade = cv2.CascadeClassifier('../haarcascades/haarcascade_eye.xml')
 
-img = cv2.imread('이길여.jpg"')
+img = cv2.imread('이길여총장님.jpg"')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 faces = face_cascade.detectMultiScale(gray, 1.3,5)
 imgNum = 0
