9b1950574d94c96bbeeef065b3fdffe8150e6441
ahnsoobin <ahnsoobin6870@gmail.com>
2023-12-13 12:54:10 +0900

    add new code

--- game_source_code.py
+++ game_source_code.py
@@ -83,3 +83,95 @@ class Player(pygame.sprite.Sprite):
                 self.rect.move_ip(5, 0)
                 position_p = self.rect.center
                 return position_p # 이 부분은 왜 필요한 건지 모르겠지만 일단 지우지 않았음. (추후에 필요할까봐)
+
+###### 게임 설정 ########
+# 플레이어 및 적 개체 생성
+P1 = Player()
+
+E1 = Enemy()
+
+# Sprites Groups 생성하기
+# 게임 물체들을 그룹화 하여 그룹별로 접근하여 설정 시 용이하게 만들기
+# 적(enemy) 객체 그룹화하기
+Enemies = pygame.sprite.Group()
+Enemies.add(E1)
+# 전체 그룹을 묶기
+All_groups = pygame.sprite.Group()
+All_groups.add(P1)
+All_groups.add(E1)
+
+# 적 개체 2초(2000ms)마다 새로 생기는 이벤트 생성
+increaseSpeed = pygame.USEREVENT + 1
+pygame.time.set_timer(increaseSpeed, 2000)
+
+# 게임 BGM 설정
+bgm = pygame.mixer.Sound('background_sound.mp3')
+bgm.play()
+
+## 게임 루프 설정 ##
+# 게임 종료되기 전까지 실행되는 루프(이벤트) 설정
+while True:
+    for event in pygame.event.get():
+        #속도 증가
+        if event.type == increaseSpeed:
+            SPEED += 0.5
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+
+    # 배경화면 이미지 삽입
+    GameDisplay.blit(background, (0, 0))
+    # 하단부에 위치할 스코어 점수(적을 피할때마다 +1점 증가)
+    scores = small_font.render("Score: " + str(SCORE), True, BLACK)
+    GameDisplay.blit(scores, (5, 100))
+
+    # group1 = '<Player Sprite(in 1 groups)>'
+    # group2 = '<Enemy Sprite(in 2 groups)>'
+
+    # 게임 내 물체 움직임 생성
+    for i in All_groups:
+        GameDisplay.blit(i.image, i.rect)
+        i.move()
+        if str(i) == '<Player Sprite(in 1 groups)>':
+            player_pos = i
+        else:
+            enemy_pos = i
+
+    # <Player Sprite(in 1 groups)>
+    # 플레이어 충돌 판정(게임종료)시
+    if pygame.sprite.spritecollideany(P1, Enemies):
+        for i in All_groups:
+            i.kill()
+        # 물체 이미지 변경(충돌후 변경되는 이미지)
+        # 플레이어
+        GameDisplay.blit(background, (0, 0))
+        image0 = pygame.image.load('user_Warning.jpg')
+        image0.get_rect()
+        GameDisplay.blit(image0, player_pos)
+
+        # 폭탄
+        image1 = pygame.image.load('bomb_Warning.jpg')
+        image1.get_rect()
+        GameDisplay.blit(image1, enemy_pos)
+        pygame.display.update()
+
+        # 배경음악 멈춤
+        bgm.stop()
+        # 적과 충돌시 효과음 추가
+        pygame.mixer.Sound('boom.mp3').play()
+        time.sleep(0.5)
+        # 게임오버화면 설정
+        pygame.mixer.Sound('boom.mp3').play()
+        GameDisplay.fill(WHITE)
+        final_scores = font.render("Your Score: " + str(SCORE), True, BLACK)
+        GameDisplay.blit(final_scores, (100, 300))
+        GameDisplay.blit(game_over, (200, 400))
+        time.sleep(1)
+        pygame.display.update()
+        time.sleep(5)
+        pygame.quit()
+        sys.exit()
+
+    pygame.display.update()
+    # 초당 프레임 설정
+    FramePerSec.tick(FPS)
